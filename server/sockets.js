9740a085f27e7a355d9ddf690aa701c8910774e3
rizvified <rizvvified@gmail.com>
2017-07-19 15:16:47 +0500

    messages working!

--- server/sockets.js
+++ server/sockets.js
@@ -1,44 +1,4 @@
-// Keep track of which names are used so that there are no duplicates
-const userNames = (function () {
-  const names = {};
-
-  const claim = (name) => {
-    if (!name || names[name]) {
-      return false;
-    }
-    names[name] = true;
-    return true;
-  };
-
-  // find the lowest unused "guest" name and claim it
-  const getGuestName = () => {
-    let name;
-    let nextUserId = 0;
-
-    do {
-      name = `Guest ${nextUserId}`;
-      nextUserId += 1;
-    } while (!claim(name));
-
-    return name;
-  };
-
-  // serialize claimed names as an array
-  const get = () => {
-    const res = [];
-    for (user in names) {
-      res.push(user);
-    }
-
-    return res;
-  };
-
-  return {
-    get,
-    getGuestName,
-  };
-}());
-
+const userNames = require('../utils/helpers');
 
 module.exports = (socket) => {
   const name = userNames.getGuestName();
